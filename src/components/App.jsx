a775c43efd1d2f6b4f7c18c38a6b999e87a578c3
BubblePenguin <kotov.senpai@gmail.com>
2023-06-20 13:25:11 +0300

    implimented profile & statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import user from '../json/user.json';
+import data from '../json/data.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile {...user} />
+      <Statistics title="what" stats={data} />
     </div>
   );
 };
